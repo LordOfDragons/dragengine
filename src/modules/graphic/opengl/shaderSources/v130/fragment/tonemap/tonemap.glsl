c3e05d6f6b315253faa37d9653c513eb98e47728
Plüss Roland <roland@rptd.ch>
2020-11-02 02:03:48 +0100

    opengl module: replace reinhard tone map operator with uchimura operator to deal with the various problems present with reinhard

--- src/modules/graphic/opengl/shaderSources/v130/fragment/tonemap/tonemap.glsl
+++ src/modules/graphic/opengl/shaderSources/v130/fragment/tonemap/tonemap.glsl
@@ -17,74 +17,70 @@ const ivec2 tcParams = ivec2( 0, 0 );
 const vec3 lumiFactors = vec3( 0.2125, 0.7154, 0.0721 );
 //const vec3 lumiFactors = vec3( 0.3086, 0.6094, 0.0820 ); // nVidia
 
+float uchimura(float x, float P, float a, float m, float l, float c, float b) {
+	// Uchimura 2017, "HDR theory and practice"
+	// Math: https://www.desmos.com/calculator/gslcdxvipg
+	// Source: https://www.slideshare.net/nikuque/hdr-theory-and-practicce-jp
+	float l0 = ((P - m) * l) / a;
+	float L0 = m - m / a;
+	float L1 = m + (1.0 - m) / a;
+	float S0 = m + l0;
+	float S1 = m + a * l0;
+	float C2 = (a * P) / (P - S1);
+	float CP = -C2 / P;
+	
+	float w0 = 1.0 - smoothstep(0.0, m, x);
+	float w2 = step(m + l0, x);
+	float w1 = 1.0 - w0 - w2;
+	
+	float T = m * pow(x / m, c) + b;
+	float S = P - (P - S1) * exp(CP * (x - S0));
+	float L = m + a * (x - m);
+	
+	return T * w0 + L * w1 + S * w2;
+}
+
+float uchimura(float x) {
+	const float P = 1.0;  // max display brightness
+	const float a = 1.0;  // contrast
+	const float m = 0.22; // linear section start
+	const float l = 0.4;  // linear section length
+	const float c = 1.33; // black
+	const float b = 0.0;  // pedestal
+	return uchimura(x, P, a, m, l, c, b);
+}
+
 void main( void ){
 	ivec2 tc = ivec2( gl_FragCoord.xy );
 	
 	vec3 params = texelFetch( texToneMapParams, tcParams, 0 ).rgb; // r=avgLum, g=scaleLum, b=lwhite, a=n/a
 	vec4 color = texelFetch( texColor, tc, 0 );
 	
-	// calculate luminance
-//	float luminance = dot( color, lumiFactors );
-//	float finalLuminance = luminance * pOptions.x;
-//	finalLuminance = finalLuminance / ( 1.0 + finalLuminance );
-	
 	// classic reinhard
 //	float luminance = dot( color, lumiFactors ) * pOptions.x;
 //	luminance = luminance / ( 1.0 + luminance );
 	
 	// enhanced reinhard
-	//float scaleLum = pOptions.x;
-	//float lwhite = pOptions.y;
-/*	vec4 params = texture2D( texToneMapParams, vec2( 0.5, 0.5 ) ); // r=luminance, g=scaleLum, b=lwhite, a=n/a
-	float scaleLum = params.g;
-	float lwhite = params.b;
+// 	color.rgb += textureLod( texBloom, min( vTCBloom, pTCBloomClamp ), 0.0 ).rgb * vec3( pOptions.x );
+// 	
+// 	float luminance = dot( color.rgb, lumiFactors );
+// 	float adjLum = luminance * params.g;
+// 	float finalLum = adjLum * ( 1.0 + adjLum * params.b ) / ( 1.0 + adjLum );
+// 	outColor = vec4( color.rgb * vec3( finalLum / ( luminance + epsilon ) ), color.a );
 	
-	float luminance = dot( color, lumiFactors ) * scaleLum;
-	//float lwhite = 1.0 / ( 2.0 * 2.0 );
-	float finalLuminance = luminance * ( 1.0 + luminance * lwhite ) / ( 1.0 + luminance );
+	// uchimura, applied per channel
+	color *= params.g;
+	outColor.r = uchimura( color.r );
+	outColor.g = uchimura( color.g );
+	outColor.b = uchimura( color.b );
+	outColor.a = color.a;
 	
-	// adaptive logaritmic mapping
-//	float luminance = dot( color, lumiFactors ) * pOptions.x;
-//	float lwmax = 2.0;
-//	float t = log( 10.0 ) / log( lwmax + 1 ); //100.0 * 0.01; // 1
-//	float b = 0.85;
-//	luminance = t * log( luminance + 1 ) / log( 2.0 + 8.0 * pow( luminance / lwmax, log( b ) / log( 0.5 ) ) );
+	// bloom
+	outColor.rgb += textureLod( texBloom, min( vTCBloom, pTCBloomClamp ), 0.0 ).rgb * vec3( pOptions.x );
 	
-	// calculate final color including overbrighting
-//	gl_FragColor.rgb = color * finalLuminance / ( luminance + 0.00001 );
-	
-//	gl_FragColor.rgb = normalize( color ) * finalLuminance;
-//	gl_FragColor.rgb = color * ( finalLuminance / luminance ) + vec3( max( ( luminance - pOptions.z ) * pOptions.w, 0.0 ) );
-//	gl_FragColor.rgb = sqrt( color * ( finalLuminance / luminance ) );
-//	gl_FragColor.rgb = sqrt( color * ( pOptions.x * finalLuminance ) );
-//	gl_FragColor.rgb = color * ( pOptions.x * finalLuminance );
-	gl_FragColor.rgb = color * scaleLum * sqrt( finalLuminance );
-	//	smoothstep( pOptions.z, 1.0, luminance ) );
-	*/
-	
-	
-	
-	color.rgb += textureLod( texBloom, min( vTCBloom, pTCBloomClamp ), 0.0 ).rgb * vec3( pOptions.x );
-	
-	float luminance = dot( color.rgb, lumiFactors );
-	float adjLum = luminance * params.g;
-	float finalLum = adjLum * ( 1.0 + adjLum * params.b ) / ( 1.0 + adjLum );
-	//float finalLum = adjLum / ( 1.0 + adjLum );
-	outColor = vec4( color.rgb * vec3( finalLum / ( luminance + epsilon ) ), color.a );
-	//outColor = vec4( ( color.rgb / vec3( luminance + epsilon ) ) * finalLum, color.a );
-	
-//	outColor.rgb += texture( texBloom, vTCBloom ).rgb * vec3( pOptions.x );// * ( finalLum - pOptions.x;
-	
-	//outColor.rgb += texture( texBloom, vTexCoord ).rgb;
-	
-	//vec4 params = texture2D( texToneMapParams, vec2( 0.0, 0.0 ) ); // r=avgLum, g=scaleLum, b=lwhite, a=n/a
-	//float scaleLum = params.g;
-	/*float threshold = 0.8; // m$(5)
-	float offset = 5.0; // m$(10)
-	
-	color = max( color * scaleLum - threshold, 0.0 );
-	gl_FragColor.rgb = color / ( color + offset );*/
-	
-	//float threshold = 0.8;
-	//gl_FragColor.rgb = max( color.rgb * scaleLum - threshold, 0.0 ) / threshold;
+	// NOTE nice comparison of curves: https://www.shadertoy.com/view/WdjSW3
+	//      the uchimura curve is the only one which has a linear left side. all others try
+	//      to add burning out the blacks which is annoying. the tone map operator should
+	//      provide a balance output. if an artist wants burning out blacks he should do
+	//      so after the tone mapping is done (still float buffer)
 }
