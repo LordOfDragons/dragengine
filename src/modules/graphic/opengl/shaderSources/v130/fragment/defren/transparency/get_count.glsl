025539a2d1c4c7f01346cb8cd43d49f42edae181
Plüss Roland <roland@rptd.ch>
2022-07-31 18:50:28 +0200

    opengl: fixed wrong transparent counter value (1 too much)

--- src/modules/graphic/opengl/shaderSources/v130/fragment/defren/transparency/get_count.glsl
+++ src/modules/graphic/opengl/shaderSources/v130/fragment/defren/transparency/get_count.glsl
@@ -13,6 +13,8 @@ out lowp float outDummy;
 
 void main( void ){
 	// the 0.1 more than 255 is only use to ensure rounding
-	if( int( texelFetch( texValues, ivec3( 0, 0, vLayer ), 0 ).r * 255.1 ) < int( gl_FragCoord.x ) ) discard;
+	if( int( texelFetch( texValues, ivec3( 0, 0, vLayer ), 0 ).r * 255.1 ) <= int( gl_FragCoord.x ) ){
+		discard;
+	}
 	outDummy = 1;
 }
