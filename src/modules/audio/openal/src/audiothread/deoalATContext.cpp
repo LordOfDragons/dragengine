dc133f8bed2d4f3193e7e97803c5a9cac634e98a
Plüss Roland <roland@rptd.ch>
2022-12-09 23:08:33 +0100

    openal: update to 1.21.1 and increase aux sends (needs more fixes)

--- src/modules/audio/openal/src/audiothread/deoalATContext.cpp
+++ src/modules/audio/openal/src/audiothread/deoalATContext.cpp
@@ -94,7 +94,7 @@ void deoalATContext::CreateContext(){
 	
 	if( extensions.GetHasEFX() ){
 		attributes[ index++ ] = ALC_MAX_AUXILIARY_SENDS;
-		attributes[ index++ ] = 8;
+		attributes[ index++ ] = 64;
 			// NOTE a send can have both an effect and a filter. to do environment simulation
 			//      it is enough to have a reverb effect and a low-pass filter. thus 1 send
 			//      is enough to simulate 1 indirect sound path
