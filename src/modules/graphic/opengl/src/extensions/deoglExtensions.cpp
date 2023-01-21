a451dcb94d984e0c1a8f960d2d72faa9c91a57fd
Plüss Roland <roland@rptd.ch>
2023-01-21 03:16:26 +0100

    opengl: is this still required?

--- src/modules/graphic/opengl/src/extensions/deoglExtensions.cpp
+++ src/modules/graphic/opengl/src/extensions/deoglExtensions.cpp
@@ -612,15 +612,18 @@ void deoglExtensions::pScanExtensions(){
 	// shader compilation to fail. sometimes OpenGL is a huge mess. why expose the presence
 	// of gl_DrawID using an extension on a lower core version if a higher core version is
 	// mandatory to use it?
+	/*
 	if( pHasExtension[ ext_ARB_shader_draw_parameters ] && pGLVersion < evgl4p6 ){
 		pRenderThread.GetLogger().LogWarn( "Extension ARB_shader_draw_parameters forcefully"
 			" disabled since OpenGL is less then 4.6 Core" );
 		pHasExtension[ ext_ARB_shader_draw_parameters ] = false;
 	}
+	*/
 	
 	// the same problem for gl_Layer in vertex shaders. looks like core 4.5 is required
 	// otherwise it fails. maybe later on create a bug-test to figure out if these two
 	// parameters are supported so we do not have to hard-code things here
+	/*
 	if( pHasExtension[ ext_ARB_shader_viewport_layer_array ] && pGLVersion < evgl4p5 ){
 		pRenderThread.GetLogger().LogWarn( "Extension ARB_shader_viewport_layer_array forcefully"
 			" disabled since OpenGL is less then 4.5 Core" );
@@ -631,6 +634,7 @@ void deoglExtensions::pScanExtensions(){
 			" disabled since OpenGL is less then 4.5 Core" );
 		pHasExtension[ ext_AMD_vertex_shader_layer ] = false;
 	}
+	*/
 	
 	pHasSeamlessCubeMap = pHasExtension[ ext_ARB_seamless_cube_map ]
 		|| pHasExtension[ ext_AMD_seamless_cubemap_per_texture ];
