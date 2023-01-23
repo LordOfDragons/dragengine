fbd1abda8c6696b689bc5c30c6dd32316541e3dd
Plüss Roland <roland@rptd.ch>
2023-01-23 17:45:14 +0100

    github actions

---
+++ vs/dragengine/pre_build.ps1
@@ -0,0 +1,6 @@
+﻿param (
+    [Parameter(Mandatory=$true)][string]$SourceDir
+)
+
+Write-Host "Drag[en]gine: Init configuration header"
+Copy-Item -Path "dragengine_configuration.h" -Destination (Join-Path -Path $SourceDir -ChildPath "dragengine_configuration.h") -Force
