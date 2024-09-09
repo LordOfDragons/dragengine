fd97f34d6b1b1bad5f7056633fc2848828ceaa40
Roland Plüss <roland@rptd.ch>
2024-09-09 12:12:12 +0200

    debian build

--- debianBuild.sh
+++ debianBuild.sh
@@ -7,15 +7,15 @@ apt update -y -q \
   && apt-get -y -q install software-properties-common \
   && add-apt-repository -y -u ppa:rpluess/dragondreams \
   && apt-get -y -q upgrade \
-  && apt-get -y -q install libdscript-dev
+  && apt-get -y -q install libdscript-dev || exit 1
 
 export SCONSFLAGS="-j 8"
 
-git clean -dfx
+git clean -dfx || exit 1
 
 scons lib_eossdk_fetch lib_fox_fetch lib_liburing_fetch \
   lib_modio_fetch lib_openxr_fetch lib_steamsdk_fetch \
-  lib_libapng_fetch
+  lib_libapng_fetch || exit 1
 
 FILE=debian/source/include-binaries
 echo "detesting/data/content/model/box/box2.demodel" >$FILE
@@ -38,13 +38,13 @@ rm -rf /sources/dragengine_*-ppa*
 # required since noble or git fails
 git config --global --add safe.directory /sources/dragengine
 
-gbp export-orig --upstream-tree=debian --force-create
+gbp export-orig --upstream-tree=debian --force-create || exit 1
 
 # gbp does not include the downloaded files in the source archive. fix it
 FILE=`cd .. && dir -1 dragengine_*.orig.tar.gz`
 FILENOEXT=`echo $FILE | sed -e "s/.orig.tar.gz//" | sed -e "s/_/-/"`
 
-gunzip ../dragengine_*.orig.tar.gz
+gunzip ../dragengine_*.orig.tar.gz || exit 1
 tar --transform "s@^\(extern.*\)@$FILENOEXT/\\1@" -rf ../dragengine_*.orig.tar \
   extern/eossdk/eossdk.zip \
   extern/eossdk/eossdk_bin_linux.tar.xz \
@@ -55,8 +55,8 @@ tar --transform "s@^\(extern.*\)@$FILENOEXT/\\1@" -rf ../dragengine_*.orig.tar \
   `dir -1 extern/openxr/OpenXR-SDK-release-*.tar.xz` \
   `dir -1 extern/steamsdk/steamsdk160.tar.xz` \
   extern/mingw/mingw_stdthreads.tar.bz2 \
-  `dir -1 extern/libapng/libpng-*tar.bz2`
-gzip ../dragengine_*.orig.tar
+  `dir -1 extern/libapng/libpng-*tar.bz2` || exit 1
+gzip ../dragengine_*.orig.tar || exit 1
 
 find -type d -name "__pycache__" | xargs -- rm -rf
 rm -f config.log
@@ -64,6 +64,6 @@ rm -f build.log
 rm -rf .sconf_temp
 rm -f .sconsign.dblite
 
-debuild -S -sa
+debuild -S -sa || exit 1
 
-dput ppa:rpluess/dragondreams /sources/dragengine_*_source.changes
+dput ppa:rpluess/dragondreams /sources/dragengine_*_source.changes || exit 1
