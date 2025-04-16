01557b4473fbcca30231844539e500593d595962
Plüss Roland <roland@rptd.ch>
2025-04-16 03:57:28 +0200

    eossdk: update to 1.17.0

--- vs/extern/eossdk/build.ps1
+++ vs/extern/eossdk/build.ps1
@@ -6,14 +6,15 @@
 
 Import-Module "$PSScriptRoot\..\..\shared.psm1"
 
+$EosSdkVersion = "1.17.0"
 
 $ExpandedDir = Join-Path -Path $ProjectDir -ChildPath "eossdk"
 if (Test-Path $ExpandedDir) {
     Remove-Item $ExpandedDir -Force -Recurse
 }
 
-DownloadArtifact -SourceDir $ProjectDir -FilenameArtifact "eossdk.zip" -UrlPath "eossdk"
-DownloadArtifact -SourceDir $ProjectDir -FilenameArtifact "eossdk_bin_windows.tar.xz" -UrlPath "eossdk"
+DownloadArtifact -SourceDir $ProjectDir -FilenameArtifact "eossdk-$EosSdkVersion.zip" -UrlPath "eossdk"
+DownloadArtifact -SourceDir $ProjectDir -FilenameArtifact "eossdk_bin_windows-$EosSdkVersion.tar.xz" -UrlPath "eossdk"
 
-Expand-Archive -Path "$ProjectDir\eossdk.zip" -DestinationPath "$ProjectDir\eossdk"
-Expand-TarXz -Path "$ProjectDir\eossdk_bin_windows.tar.xz" -Destination "$ProjectDir\eossdk"
+Expand-Archive -Path "$ProjectDir\eossdk-$EosSdkVersion.zip" -DestinationPath "$ProjectDir\eossdk"
+Expand-TarXz -Path "$ProjectDir\eossdk_bin_windows-$EosSdkVersion.tar.xz" -Destination "$ProjectDir\eossdk"
