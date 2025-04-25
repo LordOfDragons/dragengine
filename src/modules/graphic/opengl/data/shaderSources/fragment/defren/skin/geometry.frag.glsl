93f70214124615386832a0f4fe65176d49f2d496
Plüss Roland <roland@rptd.ch>
2025-04-25 21:56:44 +0200

    changes

--- src/modules/graphic/opengl/data/shaderSources/fragment/defren/skin/geometry.frag.glsl
+++ src/modules/graphic/opengl/data/shaderSources/fragment/defren/skin/geometry.frag.glsl
@@ -139,28 +139,26 @@ const vec3 lumiFactors = vec3(0.2125, 0.7154, 0.0721);
 
 
 // functions required to be define last because they are based on stuff defined above
-#ifdef WITH_VARIATIONS
-	// NOTE: according to opengl website the layer is clamped like this:
-	//         layer = clamp(floor(texCoord.p​ + 0.5), 0, d​ - 1).
-	//       thus the texCoord.p has to be reduced by 0.5 to map the entire range, example:
-	//         layerCount = 3
-	//         texCoord.p = 0.0 .. 3.0
-	//         texCoord.p - 0.5 = -0.5 .. 2.5
-	//         clamped => -0.5 .. 0.5 => 0; 0.5 .. 1.5 => 1; 1.5 .. 2.5 => 2
-	//       this way all value have the same dominance
-	
-	vec3 tcTexVar(in vec2 tc, in int layerCount){
-		vec3 p = fract(floor(tc.sts * pVariationEnableScale.xyx + pVariationSeed.xyx) * vec3(0.1031));
-		p += dot(p, p.yzx + vec3(19.19));
-		return vec3(tc, fract((p.x + p.y) * p.z) * float(layerCount) - 0.5);
-	}
-	
-	#if 0
-	vec3 tcTexVar(in vec2 tc, in int layerCount){
-		return vec3(tc, fract(sin(dot(floor(tc) / vec2(1024) * pVariationEnableScale + pVariationSeed, vec2(12.9898, 78.233)))
-			* 43758.5453123) * float(layerCount) - 0.5);
-	}
-	#endif
+// NOTE: according to opengl website the layer is clamped like this:
+//         layer = clamp(floor(texCoord.p​ + 0.5), 0, d​ - 1).
+//       thus the texCoord.p has to be reduced by 0.5 to map the entire range, example:
+//         layerCount = 3
+//         texCoord.p = 0.0 .. 3.0
+//         texCoord.p - 0.5 = -0.5 .. 2.5
+//         clamped => -0.5 .. 0.5 => 0; 0.5 .. 1.5 => 1; 1.5 .. 2.5 => 2
+//       this way all value have the same dominance
+
+vec3 tcTexVar(in vec2 tc, in int layerCount){
+	vec3 p = fract(floor(tc.sts * pVariationEnableScale.xyx + pVariationSeed.xyx) * vec3(0.1031));
+	p += dot(p, p.yzx + vec3(19.19));
+	return vec3(tc, fract((p.x + p.y) * p.z) * float(layerCount) - 0.5);
+}
+
+#if 0
+vec3 tcTexVar(in vec2 tc, in int layerCount){
+	return vec3(tc, fract(sin(dot(floor(tc) / vec2(1024) * pVariationEnableScale + pVariationSeed, vec2(12.9898, 78.233)))
+		* 43758.5453123) * float(layerCount) - 0.5);
+}
 #endif
 
 
@@ -200,9 +198,7 @@ void main(void){
 	*/
 	
 	// calculate the screen space texture coordinates
-	#ifdef REQUIRES_TEX_COORD
-		ivec2 tc = ivec2(gl_FragCoord.xy);
-	#endif
+	ivec2 tc = ivec2(gl_FragCoord.xy);
 	
 	// test against depth texture
 	#ifdef DEPTH_TEST
