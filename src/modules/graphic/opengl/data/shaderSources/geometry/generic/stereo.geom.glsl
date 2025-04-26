7cc67d136bfada6c2f497e7236036815ce20f0e8
Plüss Roland <roland@rptd.ch>
2025-04-26 12:51:03 +0200

    Revert "modifications"
    This reverts commit 230cd0af2e1fc8f01665dc24c327ba5a33e51a03.

--- src/modules/graphic/opengl/data/shaderSources/geometry/generic/stereo.geom.glsl
+++ src/modules/graphic/opengl/data/shaderSources/geometry/generic/stereo.geom.glsl
@@ -6,35 +6,52 @@
 
 #if defined GS_RENDER_STEREO
 	#ifdef GS_INSTANCING
-		layout(triangles, invocations=2) in;
-		layout(triangle_strip, max_vertices=3) out;
+		layout( triangles, invocations=2 ) in;
+		layout( triangle_strip, max_vertices=3 ) out;
 	#else
-		layout(triangles) in;
-		layout(triangle_strip, max_vertices=6) out;
+		layout( triangles ) in;
+		layout( triangle_strip, max_vertices=6 ) out;
 	#endif
 #endif
 
-in vec2 vGSTexCoord[3];
-in vec2 vGSScreenCoord[3];
+#ifndef NO_TEXCOORD
+	in vec2 vGSTexCoord[ 3 ];
+	
+	#ifdef FULLSCREENQUAD
+		in vec2 vGSScreenCoord[ 3 ];
+	#endif
+#endif
 
-out vec2 vTexCoord;
-out vec2 vScreenCoord;
+#ifndef NO_TEXCOORD
+	out vec2 vTexCoord;
+	
+	#ifdef FULLSCREENQUAD
+		out vec2 vScreenCoord;
+	#endif
+#endif
+
 flat out int vLayer;
 
-void main(void){
+void main( void ){
 	int eye;
-#ifdef GS_INSTANCING
+	#ifdef GS_INSTANCING
 	eye = gl_InvocationID;
-#else
-	for(eye=0; eye<2; eye++){
-#endif
+	#else
+	for( eye=0; eye<2; eye++ ){
+	#endif
 		
 		int corner;
-		for(corner=0; corner<3; corner++){
-			gl_Position = gl_in[corner].gl_Position;
+		for( corner=0; corner<3; corner++ ){
+			gl_Position = gl_in[ corner ].gl_Position;
 			
-			vTexCoord = vGSTexCoord[corner];
-			vScreenCoord = vGSScreenCoord[corner];
+			#ifndef NO_TEXCOORD
+				vTexCoord = vGSTexCoord[ corner ];
+				
+				#ifdef FULLSCREENQUAD
+					vScreenCoord = vGSScreenCoord[ corner ];
+				#endif
+			#endif
+			
 			vLayer = eye;
 			
 			gl_Layer = eye;
@@ -45,7 +62,7 @@ void main(void){
 		
 		EndPrimitive();
 		
-#ifndef GS_INSTANCING
+	#ifndef GS_INSTANCING
 	}
-#endif
+	#endif
 }
