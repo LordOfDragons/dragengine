3c7f508d9842877ef2543ae36d898bb8c90db9d0
Plüss Roland <roland@rptd.ch>
2025-08-01 18:58:16 +0200

    small fix

--- src/modules/graphic/opengl/data/shaderSources/compute/generic/copy_depth.glsl
+++ src/modules/graphic/opengl/data/shaderSources/compute/generic/copy_depth.glsl
@@ -5,11 +5,11 @@ layout(local_size_x=16, local_size_y=16) in;
 
 // PROBLEM: compute shaders can not read/write depth images... U_U
 layout(rgba32f, binding=0) uniform restrict readonly image2DArray inDepth;
-layout(rgba32f, binding=1) uniform restrict writeonly restrict image2DArray outDepth;
+layout(rgba32f, binding=1) uniform restrict writeonly image2DArray outDepth;
 
 #ifdef WITH_COLOR
 layout(rgba32f, binding=2) uniform restrict readonly image2DArray inColor;
-layout(rgba32f, binding=3) uniform restrict writeonly restrict image2DArray outColor;
+layout(rgba32f, binding=3) uniform restrict writeonly image2DArray outColor;
 #endif
 
 void main(){
