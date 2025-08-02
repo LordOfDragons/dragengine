e47788a0d82aa065bad6702868ec1e3016b79084
Plüss Roland <roland@rptd.ch>
2025-08-02 17:18:07 +0200

    opengl: testing glsl to spir-v(opengl) transpilation in build system

--- src/modules/graphic/opengl/src/shaders/deoglShaderSources.cpp
+++ src/modules/graphic/opengl/src/shaders/deoglShaderSources.cpp
@@ -232,6 +232,9 @@ void deoglShaderSources::pParseShader( deLogger &logger, const decXmlElementTag 
 				DETHROW( deeInvalidParam );
 			}
 			
+		}else if(tag->GetName() == "glslVersion"){
+			// for use with build system only
+			
 		}else{
 			logger.LogWarnFormat( LOGGING_SOURCE, "shader(%i:%i): Unknown Tag %s, ignoring.",
 				tag->GetLineNumber(), tag->GetPositionNumber(), tag->GetName().GetString() );
