a5a4b89b916600a83fdc5088943235871c4b3b2b
Plüss Roland <roland@dragondreams.ch>
2025-09-05 15:46:19 +0200

    fixed openal windows build

--- vs/extern/openal/build.ps1
+++ vs/extern/openal/build.ps1
@@ -8,11 +8,56 @@
 Import-Module "$PSScriptRoot\..\..\shared.psm1"
 
 
-$ExpandedDir = Join-Path -Path $ProjectDir -ChildPath "openal-soft-1.22.2-vc64"
+$ExpandedDir = "$ProjectDir\build"
 if (Test-Path $ExpandedDir) {
     Remove-Item $ExpandedDir -Force -Recurse
 }
 
-DownloadArtifact -SourceDir $ProjectDir -FilenameArtifact "openal-soft-1.22.2-vc64.zip" -UrlPath "openal"
+$OpenALVersion = "1.24.2"
 
-Expand-Archive -Path "$ProjectDir\openal-soft-1.22.2-vc64.zip" -DestinationPath $ProjectDir
+DownloadArtifact -SourceDir $ProjectDir -FilenameArtifact "openal-soft-$OpenALVersion.tar.xz" -UrlPath "openal"
+
+Expand-TarXz -Path "$ProjectDir\openal-soft-$OpenALVersion.tar.xz" -Destination $ExpandedDir
+
+$CmakeBuildDir = "$ExpandedDir\build"
+$CmakeSourceDir = "$ExpandedDir\openal-soft-$OpenALVersion"
+$CmakeInstallDir = "$ExpandedDir\install"
+
+cmake -S "$CmakeSourceDir" -B "$CmakeBuildDir" `
+	-DALSOFT_CONFIG=OFF `
+	-DALSOFT_HRTF_DEFS=OFF `
+	-DALSOFT_NO_CONFIG_UTIL=ON `
+	-DALSOFT_EXAMPLES=OFF `
+	-DALSOFT_STATIC_LIBGCC=OFF `
+	-DALSOFT_TESTS=OFF `
+	-DALSOFT_UTILS=OFF `
+	-DALSOFT_INSTALL=ON `
+	-DALSOFT_EAX=ON `
+	-DCMAKE_BUILD_TYPE=Release `
+	-DALSOFT_REQUIRE_DSOUND=ON `
+	-DALSOFT_REQUIRE_WINMM=ON `
+	-DALSOFT_REQUIRE_WASAPI=ON `
+	-DCMAKE_POSITION_INDEPENDENT_CODE=ON `
+	-DLIBTYPE=STATIC
+
+cmake --build "$CmakeBuildDir" -- /p:Configuration=Release
+cmake --install "$CmakeBuildDir"
+
+if (!(Test-Path $CmakeInstallDir)) {
+    New-Item -Path $CmakeInstallDir -ItemType "directory"
+}
+if (!(Test-Path "$CmakeInstallDir\include")) {
+    New-Item -Path "$CmakeInstallDir\include" -ItemType "directory"
+}
+if (!(Test-Path "$CmakeInstallDir\include\AL")) {
+    New-Item -Path "$CmakeInstallDir\include\AL" -ItemType "directory"
+}
+if (!(Test-Path "$CmakeInstallDir\lib")) {
+    New-Item -Path "$CmakeInstallDir\lib" -ItemType "directory"
+}
+
+Move-Item -Path "$CmakeSourceDir\include\AL\*.h" `
+	-Destination "$CmakeInstallDir\include\AL"
+
+Move-Item -Path "$CmakeBuildDir\Release\OpenAL32.lib" `
+	-Destination "$CmakeInstallDir\lib"
