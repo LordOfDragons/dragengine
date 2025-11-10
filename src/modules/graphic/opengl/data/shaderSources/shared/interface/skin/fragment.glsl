eece587d7a670782eaba73e84c4b6dcf6bfb9ea5
Plüss Roland <roland@dragondreams.ch>
2025-11-10 20:11:12 +0100

    quest: shader compile fixes (qualifiers)

--- src/modules/graphic/opengl/data/shaderSources/shared/interface/skin/fragment.glsl
+++ src/modules/graphic/opengl/data/shaderSources/shared/interface/skin/fragment.glsl
@@ -1,22 +1,22 @@
 // usage:
 // #include "shared/interface/skin/fragment.glsl"
 
-VARYING_BIND(0) in vec4 vTexCoords1; // color, colorTintMask
-VARYING_BIND(1) in vec4 vTexCoords2; // normal, reflectivity
-VARYING_BIND(2) in vec4 vTexCoords3; // emissivity, refractionDistort
-VARYING_BIND(3) in vec2 vTexCoords4; // ao
-VARYING_BIND(4) in vec4 vNormalHTMask; // normal, HTMask
-VARYING_BIND(5) in vec4 vTangentFadeZ; // tangent, fadeZ
-VARYING_BIND(6) in vec3 vBitangent;
-VARYING_BIND(7) in vec3 vReflectDir;
-VARYING_BIND(8) in vec3 vClipCoord;
-VARYING_BIND(9) in vec3 vSkinClipCoord;
-VARYING_BIND(10) in vec3 vPosition;
-VARYING_BIND(11) flat in ivec4 vIndicesFlags; // doubleSided, SPBIndex, SPBFlags, layer
+VARYING_BIND(0) in mediump vec4 vTexCoords1; // color, colorTintMask
+VARYING_BIND(1) in mediump vec4 vTexCoords2; // normal, reflectivity
+VARYING_BIND(2) in mediump vec4 vTexCoords3; // emissivity, refractionDistort
+VARYING_BIND(3) in lowp vec2 vTexCoords4; // ao
+VARYING_BIND(4) in mediump vec4 vNormalHTMask; // normal, HTMask
+VARYING_BIND(5) in mediump vec4 vTangentFadeZ; // tangent, fadeZ
+VARYING_BIND(6) in mediump vec3 vBitangent;
+VARYING_BIND(7) in mediump vec3 vReflectDir;
+VARYING_BIND(8) in mediump vec3 vClipCoord;
+VARYING_BIND(9) in mediump vec3 vSkinClipCoord;
+VARYING_BIND(10) in mediump vec3 vPosition;
+VARYING_BIND(11) flat in mediump ivec4 vIndicesFlags; // doubleSided, SPBIndex, SPBFlags, layer
 
-VARYING_BIND(12) in vec3 vParticle0; // size, emissivity, rotation
-VARYING_BIND(13) in vec4 vParticle1; // red, green, blue, transparency
-VARYING_BIND(14) flat in int vParticleSheetCount;
+VARYING_BIND(12) in mediump vec3 vParticle0; // size, emissivity, rotation
+VARYING_BIND(13) in lowp vec4 vParticle1; // red, green, blue, transparency
+VARYING_BIND(14) flat in lowp int vParticleSheetCount;
 
 #define vTCColor (vTexCoords1.xy)
 #define vTCColorTintMask (vTexCoords1.zw)
--- src/modules/graphic/opengl/data/shaderSources/shared/interface/skin/out_geometry.glsl
+++ src/modules/graphic/opengl/data/shaderSources/shared/interface/skin/out_geometry.glsl
@@ -1,19 +1,19 @@
-VARYING_BIND(0) out vec4 vGSTexCoords1; // color, colorTintMask
-VARYING_BIND(1) out vec4 vGSTexCoords2; // normal, reflectivity
-VARYING_BIND(2) out vec4 vGSTexCoords3; // emissivity, refractionDistort
-VARYING_BIND(3) out vec2 vGSTexCoords4; // ao
-VARYING_BIND(4) out vec4 vGSNormalHTMask; // normal, HTMask
-VARYING_BIND(5) out vec4 vGSTangentFadeZ; // tangent, fadeZ
-VARYING_BIND(6) out vec3 vGSBitangent;
-VARYING_BIND(7) out vec3 vGSReflectDir;
-VARYING_BIND(8) out vec3 vGSClipCoord;
-VARYING_BIND(9) out vec3 vGSSkinClipCoord;
-VARYING_BIND(10) out vec3 vGSPosition;
-VARYING_BIND(11) flat out ivec4 vGSIndicesFlags; // doubleSided, SPBIndex, SPBFlags, layer
+VARYING_BIND(0) out mediump vec4 vGSTexCoords1; // color, colorTintMask
+VARYING_BIND(1) out mediump vec4 vGSTexCoords2; // normal, reflectivity
+VARYING_BIND(2) out mediump vec4 vGSTexCoords3; // emissivity, refractionDistort
+VARYING_BIND(3) out lowp vec2 vGSTexCoords4; // ao
+VARYING_BIND(4) out mediump vec4 vGSNormalHTMask; // normal, HTMask
+VARYING_BIND(5) out mediump vec4 vGSTangentFadeZ; // tangent, fadeZ
+VARYING_BIND(6) out mediump vec3 vGSBitangent;
+VARYING_BIND(7) out mediump vec3 vGSReflectDir;
+VARYING_BIND(8) out mediump vec3 vGSClipCoord;
+VARYING_BIND(9) out mediump vec3 vGSSkinClipCoord;
+VARYING_BIND(10) out mediump vec3 vGSPosition;
+VARYING_BIND(11) flat out mediump ivec4 vGSIndicesFlags; // doubleSided, SPBIndex, SPBFlags, layer
 
-VARYING_BIND(12) out vec3 vGSParticle0; // size, emissivity, rotation
-VARYING_BIND(13) out vec4 vGSParticle1; // red, green, blue, transparency
-VARYING_BIND(14) flat out int vGSParticleSheetCount;
+VARYING_BIND(12) out mediump vec3 vGSParticle0; // size, emissivity, rotation
+VARYING_BIND(13) out lowp vec4 vGSParticle1; // red, green, blue, transparency
+VARYING_BIND(14) flat out lowp int vGSParticleSheetCount;
 
 #define vTexCoords1 vGSTexCoords1
 #define vTexCoords2 vGSTexCoords2
