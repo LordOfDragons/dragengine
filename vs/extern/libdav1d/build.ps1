b72e1980509be6c1e191779c4cd6197ca85fb96e
Plüss Roland <roland@dragondreams.ch>
2026-08-16 18:06:58 +0200

    libdav1d: fixed wrong version

--- vs/extern/libdav1d/build.ps1
+++ vs/extern/libdav1d/build.ps1
@@ -12,7 +12,7 @@ if (Test-Path "$PSScriptRoot\..\..\github_cached_externals") {
     return
 }
 
-$LibVersion = "1.4.1"
+$LibVersion = "1.5.4"
 
 $ExpandedDir = Join-Path -Path $ProjectDir -ChildPath "build"
 if (Test-Path $ExpandedDir) {
